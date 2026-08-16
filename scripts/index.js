48f971b28aa6b2d17f390ed9329c11b909c04b55
nin25-08 <315899434+nin25-08@users.noreply.github.com>
2026-08-16 18:39:39 +0200

    Reviewed logic to add custom UI with different buttons

--- scripts/index.js
+++ scripts/index.js
@@ -19,34 +19,83 @@ function getHumanChoice() {
 }
 
 
-function playRound(computerChoice,humanChoice){
-    if(humanChoice=="invalid"){
+function playRound(computerChoice, humanChoice) {
+    if (humanChoice == "invalid") {
         return -1
     }
-    switch(humanChoice){
-        case "rock":return(computerChoice=="scissors")?0:1
-        case "paper":return(computerChoice=="rock")?0:1
-        case "scissors":return(computerChoice=="paper")?0:1
-}
-}
-function playGame(){
-    let i=0
-    let humanScore=0
-    let computerScore=0
-    while(i<5){
-    let winner=playRound(getComputerChoice(),getHumanChoice())
-    if(winner==0) {
-        console.log("Player won!")
-        humanScore++
+    switch (humanChoice) {
+        case "rock": return (computerChoice == "scissors") ? "Congratulations! You win!" : (computerChoice == "rock") ? ("It's a tie!") : ("The computer won! Try again!")
+        case "paper": return (computerChoice == "rock") ? "Congratulations! You win!" : (computerChoice == "paper") ? ("It's a tie!") : ("The computer won! Try again!")
+        case "scissors": return (computerChoice == "paper") ? "Congratulations! You win!" : (computerChoice == "scissors") ? ("It's a tie!") : ("The computer won! Try again!")
     }
-    else if(winner==1){
-        console.log("Computer won!")
-        computerScore++
-    }
-    i++
-}
-    return humanScore-computerScore
 }
 
-if(playGame()<0) console.log("Player won the game!")
-else console.log("Computer won the game!")
+
+const result = document.querySelector(".result")
+
+const rockbtn = document.querySelector(".rock")
+const scissorsbtn = document.querySelector(".scissors")
+const paperbtn = document.querySelector(".paper")
+
+const buttons = document.querySelectorAll("button")
+
+const usrScore = document.querySelector(".userScore")
+const computerScore = document.querySelector(".computerScore")
+let usr = 0
+let computer = 0
+let roundsPlayed=0
+
+document.addEventListener("gameOver",()=>{
+    const gameOverMessage = document.createElement("h2");
+    gameOverMessage.textContent = "Game Over! 5 Rounds Complete.";
+    result.appendChild(gameOverMessage);
+    buttons.forEach(button => {
+        button.disabled = true;
+    });
+})
+rockbtn.addEventListener("click", () => {
+    result.textContent = ""
+    const winner = playRound(getComputerChoice(), "rock")
+    result.appendChild(handlePlay(winner))
+    roundsPlayed++
+    isGameOver(roundsPlayed)
+})
+scissorsbtn.addEventListener("click", () => {
+    result.textContent = ""
+    const winner = playRound(getComputerChoice(), "scissors")
+    result.appendChild(handlePlay(winner))
+    roundsPlayed++
+    isGameOver(roundsPlayed)
+})
+paperbtn.addEventListener("click", () => {
+    result.textContent = ""
+    const winner = playRound(getComputerChoice(), "paper")
+    result.appendChild(handlePlay(winner))
+    roundsPlayed++
+    isGameOver(roundsPlayed)
+
+})
+
+function handlePlay(winner) {
+    switch (winner) {
+        case ("Congratulations! You win!"):
+            usrScore.textContent = (++usr).toString()
+            break
+        case ("It's a tie!"):
+            usrScore.textContent = (++usr).toString();
+            computerScore.textContent = (++computer).toString()
+            break
+        case ("The computer won! Try again!"):
+            computerScore.textContent = (++computer).toString()
+            break
+    }
+    const p = document.createElement("p")
+    p.textContent = winner
+    return p
+}
+function isGameOver(games){
+    if(games>=5){
+        document.dispatchEvent(new CustomEvent("gameOver"))
+    }
+
+}
